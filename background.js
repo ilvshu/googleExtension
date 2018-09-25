6e0457ffb67a5945eae518d286f0ab0fff6483d3
xuwh <906444214@qq.com>
2018-09-25 09:13:45 +0800

    修改

--- background.js
+++ background.js
@@ -13,15 +13,39 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendRequest){
 function crateTable() {
   var sql = 'create table if not exists t_content(text, url, create_time)';
   db.transaction(function(tx) {
-  　　tx.executeSql(sql);
+  　　tx.executeSql(sql); 
   });
 }
 
 
 function addContent(content) {
+  content.text = replaceStr(content.text);
 	 var currentDate = new Date().toLocaleString();
-     var sql = 'INSERT INTO t_content (text, url, create_time) VALUES ("'+content.text+'","'+content.url+'","'+currentDate+'")';
+     var sql = "INSERT INTO t_content (text, url, create_time) VALUES ('"+content.text+"','"+content.url+"','"+currentDate+"')";
+     console.info(sql);
      db.transaction(function(tx) {
      　　tx.executeSql(sql);
+    },
+    function (tx) {
+        alert('插入失败: ' + tx.message);
     });
 }
+
+
+function findContent(content){
+  var sql = 'SELECT * FROM t_content WHERE text = '
+}
+
+
+
+function replaceStr(keyWord){
+  keyWord = keyWord.replaceAll("'", "''");
+  return keyWord;
+}
+
+
+String.prototype.replaceAll  = function(s1,s2){     
+    return this.replace(new RegExp(s1,"gm"),s2);     
+}   
+
+
